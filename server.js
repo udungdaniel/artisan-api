c9ea904f8e8562aea55accd63eda4e5c03603f13
udungdaniel <danielalfred783@gmail.com>
2026-05-25 23:53:18 +0100

    update

--- server.js
+++ server.js
@@ -1,5 +1,8 @@
 const express = require('express');
 const dotenv = require('dotenv');
+const swaggerUi = require('swagger-ui-express');
+
+const swaggerDocument = require('./swagger');
 
 dotenv.config();
 
@@ -7,11 +10,18 @@ const app = express();
 
 app.use(express.json());
 
-app.use('/artisans', require('./routes/artisans'));
-app.use('/bookings', require('./routes/bookings'));
+// HOME ROUTE
 app.get('/', (req, res) => {
   res.send('Artisan Service API is running...');
 });
+
+// SWAGGER ROUTE
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+
+// ROUTES
+app.use('/artisans', require('./routes/artisans'));
+app.use('/bookings', require('./routes/bookings'));
+
 const port = process.env.PORT || 3000;
 
 app.listen(port, () => {
