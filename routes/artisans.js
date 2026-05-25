a8be2659a996cbd1cc86c367b3ff3b3d79ac8f04
udungdaniel <danielalfred783@gmail.com>
2026-05-26 00:00:44 +0100

    Update

--- routes/artisans.js
+++ routes/artisans.js
@@ -4,14 +4,77 @@ const router = express.Router();
 
 const artisansController = require('../controllers/artisans');
 
+/**
+ * @swagger
+ * /artisans:
+ *   get:
+ *     summary: Get all artisans
+ *     responses:
+ *       200:
+ *         description: Success
+ */
 router.get('/', artisansController.getAll);
 
+/**
+ * @swagger
+ * /artisans/{id}:
+ *   get:
+ *     summary: Get single artisan
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         required: true
+ *         schema:
+ *           type: string
+ *     responses:
+ *       200:
+ *         description: Success
+ */
 router.get('/:id', artisansController.getSingle);
 
+/**
+ * @swagger
+ * /artisans:
+ *   post:
+ *     summary: Create artisan
+ *     responses:
+ *       201:
+ *         description: Artisan created
+ */
 router.post('/', artisansController.createArtisan);
 
+/**
+ * @swagger
+ * /artisans/{id}:
+ *   put:
+ *     summary: Update artisan
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         required: true
+ *         schema:
+ *           type: string
+ *     responses:
+ *       204:
+ *         description: Artisan updated
+ */
 router.put('/:id', artisansController.updateArtisan);
 
+/**
+ * @swagger
+ * /artisans/{id}:
+ *   delete:
+ *     summary: Delete artisan
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         required: true
+ *         schema:
+ *           type: string
+ *     responses:
+ *       200:
+ *         description: Artisan deleted
+ */
 router.delete('/:id', artisansController.deleteArtisan);
 
 module.exports = router;
--- routes/bookings.js
+++ routes/bookings.js
@@ -4,14 +4,77 @@ const router = express.Router();
 
 const bookingsController = require('../controllers/bookings');
 
+/**
+ * @swagger
+ * /bookings:
+ *   get:
+ *     summary: Get all bookings
+ *     responses:
+ *       200:
+ *         description: Success
+ */
 router.get('/', bookingsController.getAll);
 
+/**
+ * @swagger
+ * /bookings/{id}:
+ *   get:
+ *     summary: Get single booking
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         required: true
+ *         schema:
+ *           type: string
+ *     responses:
+ *       200:
+ *         description: Success
+ */
 router.get('/:id', bookingsController.getSingle);
 
+/**
+ * @swagger
+ * /bookings:
+ *   post:
+ *     summary: Create booking
+ *     responses:
+ *       201:
+ *         description: Booking created
+ */
 router.post('/', bookingsController.createBooking);
 
+/**
+ * @swagger
+ * /bookings/{id}:
+ *   put:
+ *     summary: Update booking
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         required: true
+ *         schema:
+ *           type: string
+ *     responses:
+ *       204:
+ *         description: Booking updated
+ */
 router.put('/:id', bookingsController.updateBooking);
 
+/**
+ * @swagger
+ * /bookings/{id}:
+ *   delete:
+ *     summary: Delete booking
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         required: true
+ *         schema:
+ *           type: string
+ *     responses:
+ *       200:
+ *         description: Booking deleted
+ */
 router.delete('/:id', bookingsController.deleteBooking);
 
 module.exports = router;
